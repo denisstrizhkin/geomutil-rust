4d7f57d7484841ea23609fc28f9fe22e89969227
Denis Strizhkin <strdenis02@gmail.com>
2025-10-25 10:40:30 +0300

    make Float trait pub

--- geomutil_util/src/lib.rs
+++ geomutil_util/src/lib.rs
@@ -8,5 +8,6 @@ mod triangle;
 pub use bounding_box::{BoundingBox2, BoundingBox3};
 pub use edge::{Edge2, Edge3};
 pub use point::{Point2, Point3};
+pub use scalar::Float;
 pub use shape2::Shape2D;
 pub use triangle::Triangle;
